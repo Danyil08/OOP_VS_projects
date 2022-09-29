b74f1734ea63037f35d9dae6349c261f1e60f317
Danyil08 <73671039+Danyil08@users.noreply.github.com>
2022-09-29 16:45:49 +0300

    Add files via upload

--- Input_and_output_tasks/Program.cs
+++ Input_and_output_tasks/Program.cs
@@ -1,24 +1,22 @@
 ﻿using System;
-using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace ConsoleApp1
+namespace WindowsFormsApp1
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            Console.OutputEncoding = UTF8Encoding.UTF8;
-            string s = Console.ReadLine();
-            try
-            {
-                int x = Convert.ToInt32(s);
-                Console.WriteLine($"Ви ввели число {x}");
-            }
-            catch
-            {
-                Console.WriteLine("N не є цілим числом");
-            }
-            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
