8324de506bf120336788aa4576a7d5a42dc6fc74
Danyil08 <73671039+Danyil08@users.noreply.github.com>
2022-09-29 16:48:22 +0300

    Add files via upload

---
+++ Input_and_output_tasks/CA/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            string s = Console.ReadLine();
+            try
+            {
+                int x = Convert.ToInt32(s);
+                Console.WriteLine($"Ви ввели число {x}");
+            }
+            catch
+            {
+                Console.WriteLine("N не є цілим числом");
+            }
+            
+        }
+    }
+}
