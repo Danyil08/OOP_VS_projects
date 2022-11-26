b06d67a20f238bb1bc05fe88cf9198e027a354fb
Danyil08 <73671039+Danyil08@users.noreply.github.com>
2022-11-26 19:45:17 +0200

    Add files via upload

---
+++ Templates/Templates4/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+namespace Decorator.Examples
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            // Create ConcreteComponent and two Decorators
+            Tree tree = new Tree();
+            Decoration decoration = new Decoration();
+            Garland garland = new Garland();
+
+            // Link decorators
+            decoration.SetComponent(tree);
+            garland.SetComponent(decoration);
+
+            garland.Operation();
+
+            // Wait for user
+            Console.Read();
+        }
+    }
+    // "Component"
+    abstract class Component
+    {
+        public abstract void Operation();
+    }
+
+    // "ConcreteComponent"
+    class Tree : Component
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("Tree.Operation()");
+        }
+    }
+    // "Decorator"
+    abstract class Decorator : Component
+    {
+        protected Component component;
+
+        public void SetComponent(Component component)
+        {
+            this.component = component;
+        }
+        public override void Operation()
+        {
+            if (component != null)
+            {
+                component.Operation();
+            }
+        }
+    }
+
+    // "Decoration"
+    class Decoration : Decorator
+    {
+        private string decoration;
+
+        public override void Operation()
+        {
+            base.Operation();
+            decoration = "decoration";
+            Console.WriteLine("Decoration.Operation()");
+        }
+    }
+
+    // "Garland" 
+    class Garland : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            GarlandLight();
+            Console.WriteLine("Garland.Operation()");
+        }
+        void GarlandLight()
+        {
+            Console.WriteLine("Let be light there!");
+        }
+    }
+}
