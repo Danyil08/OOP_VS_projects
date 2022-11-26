e3fe9a6702b4ef2db618929e1c60837dca7bb3cc
Danyil08 <73671039+Danyil08@users.noreply.github.com>
2022-11-26 19:45:56 +0200

    Add files via upload

---
+++ Templates/Templates6/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade.Examples
+{
+    // Mainapp test application 
+    class MainApp
+    {
+        public static void Main()
+        {
+            Facade facade = new Facade();
+            facade.PositiveIntegersOperationsAbout();
+            facade.RationalNumbersOperationsAbout();
+            // Wait for user 
+            Console.Read();
+        }
+    }
+
+
+    // "Subsystem ClassA" 
+    class Adding
+    {
+        public void About()
+        {
+            Console.WriteLine("Adding method");
+        }
+    }
+
+    // Subsystem ClassB" 
+    class Subtraction
+    {
+        public void About()
+        {
+            Console.WriteLine("Subtraction method");
+        }
+    }
+
+
+    // Subsystem ClassC" 
+    class Multiplication
+    {
+        public void About()
+        {
+            Console.WriteLine("Multiplication method");
+        }
+    }
+    // Subsystem ClassD" 
+    class Division
+    {
+        public void About()
+        {
+            Console.WriteLine("Division method");
+        }
+    }
+    // "Facade" 
+    class Facade
+    {
+        Adding adding;
+        Subtraction subtraction;
+        Multiplication multiplication;
+        Division division;
+
+        public Facade()
+        {
+            adding = new Adding();
+            subtraction = new Subtraction();
+            multiplication = new Multiplication();
+            division = new Division();
+        }
+
+        public void PositiveIntegersOperationsAbout()
+        {
+            Console.WriteLine("\nPositiveIntegersOperationsAbout() ---- ");
+            adding.About();
+            multiplication.About();
+        }
+
+        public void RationalNumbersOperationsAbout()
+        {
+            Console.WriteLine("\nRationalNumbersOperationsAbout() ---- ");
+            adding.About();
+            subtraction.About();
+            multiplication.About();
+            division.About();
+        }
+    }
+}
